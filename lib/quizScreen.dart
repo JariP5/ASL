61931487296085613a0efb8a81c7f1b83b46d3b0
Matheus K. Westphalen <mkwestphalen@gmail.com>
2023-03-27 13:30:57 -0400

    Create quizScreen.dart

---
+++ lib/quizScreen.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import './quiz.dart';
+
+// things that still need to be done:
+// have images for each question
+// randomize which questions are asked
+class QuestionWidget extends StatefulWidget {
+  const QuestionWidget({
+    Key? key,
+  }) : super(key: key);
+
+  // should shuffle the list don't why it doesnt
+  // const questions.shuffle();
+  @override
+  State<QuestionWidget> createState() => _QuestionWidgetState();
+}
+
+class _QuestionWidgetState extends State<QuestionWidget> {
+  late PageController _controller;
+  int _questionNumber = 1;
+  int _score = 0;
+  bool _isLocked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(initialPage: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const SizedBox(height: 32),
+          Text('Question $_questionNumber/5'),
+          const Divider(thickness: 1, color: Colors.grey),
+          Expanded(
+            child: PageView.builder(
+                itemCount: questions.length,
+                controller: _controller,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final _question = questions[index];
+                  return buildQuestion(_question);
+                }),
+          ),
+          _isLocked ? buildElevatedButton() : const SizedBox.shrink(),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  Column buildQuestion(Question question) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 32),
+        Text(
+          question.text,
+          style: const TextStyle(fontSize: 25),
+        ),
+        const SizedBox(height: 32),
+        Expanded(
+          child: OptionsWidget(
+            question: question,
+            onClickedOption: (option) {
+              if (question.isLocked) {
+                return;
+              } else {
+                setState(() {
+                  question.isLocked = true;
+                  question.selectedOption = option;
+                });
+                _isLocked = question.isLocked;
+                if (question.selectedOption!.isCorrect) {
+                  _score++;
+                }
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  ElevatedButton buildElevatedButton() {
+    return ElevatedButton(
+        onPressed: () {
+          if (_questionNumber < questions.length) {
+            _controller.nextPage(
+              duration: const Duration(milliseconds: 250),
+              curve: Curves.easeInExpo,
+            );
+
+            setState(() {
+              _questionNumber++;
+              _isLocked = false;
+            });
+          } else {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ResultPage(score: _score),
+              ),
+            );
+          }
+        },
+        child: Text(
+          _questionNumber < questions.length ? 'Next Question' : 'See Results',
+        ));
+  }
+}
+
+class OptionsWidget extends StatelessWidget {
+  final Question question;
+  final ValueChanged<Option> onClickedOption;
+
+  const OptionsWidget({
+    Key? key,
+    required this.question,
+    required this.onClickedOption,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        child: Column(
+          children: question.options
+              .map((option) => buildOption(context, option))
+              .toList(),
+        ),
+      );
+
+  Widget buildOption(BuildContext context, Option option) {
+    final color = getColorForOption(option, question);
+    return GestureDetector(
+      onTap: () => onClickedOption(option),
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(12),
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          // color for the option boxs
+          color: Color(0xffcfcaca),
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: color),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              option.text,
+              style: const TextStyle(fontSize: 20),
+            ),
+            getIconForOption(option, question),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Color getColorForOption(Option option, Question question) {
+    final isSelected = option == question.selectedOption;
+    if (question.isLocked) {
+      if (isSelected) {
+        return option.isCorrect ? Colors.green : Colors.red;
+      } else if (option.isCorrect) {
+        return Colors.green;
+      }
+    }
+    return Colors.grey;
+  }
+
+  Widget getIconForOption(Option option, Question question) {
+    final isSelected = option == question.selectedOption;
+    if (question.isLocked) {
+      if (isSelected) {
+        return option.isCorrect
+            ? const Icon(Icons.check_circle, color: Colors.green)
+            : const Icon(Icons.cancel, color: Colors.red);
+      } else if (option.isCorrect) {
+        return const Icon(Icons.check_circle, color: Colors.green);
+      }
+    }
+    return const SizedBox.shrink();
+  }
+}
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({Key? key, required this.score}) : super(key: key);
+
+  final int score;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('You got $score/5'),
+      ),
+    );
+  }
+}
