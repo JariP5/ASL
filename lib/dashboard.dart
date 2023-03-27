544f0b2574c99b4f0b4821110c38bf75a7a53376
Matheus K. Westphalen <mkwestphalen@gmail.com>
2023-03-27 13:34:04 -0400

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import './quizScreen.dart';
 
 class ASLDashboard extends StatelessWidget {
-  final String title;
-  const ASLDashboard({super.key, required this.title});
-
   @override
   Widget build(BuildContext context) => Container(
           child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
         const SizedBox(height: 700),
         Padding(padding: EdgeInsets.only(top: 5)),
         ElevatedButton(
-          child: Text('Practice'),
+          child: Text('Learn'),
           onPressed: () {},
           // on press go to new page or something
         ),
         ElevatedButton(
-          child: Text('Learn'),
-          onPressed: () {},
+          child: Text('Quiz'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => QuestionWidget()),
+            );
+          },
           // on press go to new page or something
         ),
         ElevatedButton(
