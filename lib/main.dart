6779af57c7424f1fe751815892b01c2286bda5c3
Jari Polm <jpolm@rollins.edu>
2023-03-27 14:06:48 -0400

    Took firebase into own file out of main

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ml_model_downloader/firebase_ml_model_downloader.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/firebase.dart';
 
 import './dashboard.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseModelDownloader.instance
-    .getModel(
-        "LiteModel",
-        FirebaseModelDownloadType.localModel,
-        FirebaseModelDownloadConditions(
-          iosAllowsCellularAccess: true,
-          iosAllowsBackgroundDownloading: false,
-          androidChargingRequired: false,
-          androidWifiRequired: false,
-          androidDeviceIdleRequired: false,
-        )
-    )
-    .then((customModel) {
-      // Download complete. Depending on your app, you could enable the ML
-      // feature, or switch from the local model to the remote model, etc.
-
-      // The CustomModel object contains the local path of the model file,
-      // which you can use to instantiate a TensorFlow Lite interpreter.
-      final localModelPath = customModel.file;
-      print('Downloaded');
-
-      // ...
-    });
+  firebase();
   runApp(const MyApp());
 }
 
