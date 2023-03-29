3f780a8a893254c63e9c047a61e06c5a9973e6f4
aakritishah <78161415+aakritishah@users.noreply.github.com>
2023-03-28 20:28:39 -0400

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,37 +1,43 @@
+import 'package:ASL/dict.dart';
 import 'package:flutter/material.dart';
-
 import './quizScreen.dart';
+import './dict.dart';
 
 class ASLDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
           child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-        const SizedBox(height: 70),
-        Padding(padding: EdgeInsets.only(top: 5)),
+        const SizedBox(height: 100),
+        const Padding(padding: EdgeInsets.only(top: 5)),
         ElevatedButton(
           child: Text('Learn'),
           onPressed: () {},
           // on press go to new page or something
         ),
         SizedBox(
-          height: 50
+          height: 50,
         ),
         ElevatedButton(
           child: Text('Quiz'),
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => QuestionWidget()),
+              MaterialPageRoute(builder: (context) => const QuizApp()),
             );
           },
           // on press go to new page or something
         ),
         SizedBox(
-          height: 50
+          height: 50,
         ),
         ElevatedButton(
           child: Text('Dictionary'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ASLDictionaryScreen()),
+            );
+          },
           // on press go to new page or something
         ),
       ]));
