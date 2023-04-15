f85379c40655b0742537e7cd2a1c92df31a6d005
Jari Polm <jpolm@rollins.edu>
2023-04-14 21:47:56 -0400

    Add labels and model

--- lib/Learn/ui/camera_view.dart
+++ lib/Learn/ui/camera_view.dart
@@ -1,7 +1,5 @@
-//import 'package:ASL/camera_page.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
 class CameraView extends StatefulWidget {
   final CameraDescription camera;
 
