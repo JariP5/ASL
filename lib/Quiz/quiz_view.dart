0cfe7f3db0f681bd3878c66c0667a83941e04a30
Matheus K. Westphalen <mkwestphalen@gmail.com>
2023-05-09 00:18:34 -0400

    Replace ProgressBar from Quiz_View with generic in /Styles

--- lib/Quiz/quiz_view.dart
+++ lib/Quiz/quiz_view.dart
@@ -171,36 +171,6 @@ class _QuestionWidgetState extends State<QuestionWidget> {
   }
 }
 
-// class ProgressBar extends StatelessWidget {
-//   final int currentQuestion;
-//   const ProgressBar({Key? key, required this.currentQuestion})
-//       : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       clipBehavior: Clip.hardEdge,
-//       decoration: BoxDecoration(borderRadius: BorderRadius.circular(90.0)),
-//       width: MediaQuery.of(context).size.width / 1.3,
-//       height: 35,
-//       child: Stack(
-//         alignment: Alignment.centerLeft,
-//         children: [
-//           Positioned.fill(
-//             child: LinearProgressIndicator(
-//               // Here you pass the percentage that reflects how much the
-//               // the progress bar should be filled.
-//               value: currentQuestion / 5,
-//               color: kPrimaryColor,
-//               backgroundColor: kPrimaryColor.withOpacity(0.5),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 class OptionsWidget extends StatelessWidget {
   final Question question;
   final ValueChanged<Option> onClickedOption;
